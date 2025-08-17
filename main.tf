83b64a0c4eb19e40cc506ba4c97b845aac4f6cdc
krishna <dsaikrishna784@gmail.com>
2025-08-17 12:00:36 +0530

    task

--- main.tf
+++ main.tf
@@ -1,14 +1,35 @@
 provider "aws" {
-  
+  region = "eu-north-1"
 }
 
-resource "aws_instance" "dev" {
-    ami = "ami-0b83c7f5e2823d1f4"
-    instance_type = "t3.micro"
-    key_name = "intern"
-    # availability_zone = "ap-south-1a"
-    tags = {
-      Name = "dev"
-    }
+resource "aws_instance" "server" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name = var.env_name
+  }
+}
+
+variable "ami_id" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 Instance type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Key Pair name"
+  type        = string
+}
+
+variable "env_name" {
+  description = "Environment name (dev/prod)"
+  type        = string
+  default     = "dev"
 }
 
